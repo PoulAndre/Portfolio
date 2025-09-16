d4bcada2d931d4b81e8fb863deb61fefdf9c9a74
PoulAndre <poul.a@hotmail.no>
2025-09-16 20:42:01 +0200

    endringer på config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/Portfolie",
-    assetPrefix: "/Portfolie/",
+    basePath: "/Portfolio",
+    assetPrefix: "/Portfolio/",
     output: "export",
     reactStrictMode: true,
     
